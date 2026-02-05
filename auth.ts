db03809faaec4e79e26a1907567eaa6b9d54fce6
Abhinand <abhinandtk.primalcodes@gmail.com>
2026-02-05 23:54:26 +0530

    sync user completed

--- auth.ts
+++ auth.ts
@@ -6,8 +6,31 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub,Google],
   callbacks:{
     async jwt({token,user,account}) {
-        if(user){
+        if(user && account){
             token.id=user.id
+            const res = await fetch(
+          `${process.env.DJANGO_API_URL}/api/auth/sync-user/`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              email: user.email,
+              name: user.name,
+              provider: account?.provider,
+            }),
+          }
+        )
+
+        if (res.ok) {
+          const data = await res.json()
+          console.log(data,'checkingresponsedata')
+
+          // 🔐 Store Django JWT in NextAuth token
+          token.backendAccessToken = data.access
+          token.backendRefreshToken = data.refresh
+        } else {
+          console.error("Failed to sync user with backend", await res.text())
+        }
         }
         if(account){
             token.accessToken=account.access_token
@@ -20,6 +43,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         }
         if(token?.accessToken){
             session.accessToken=token.accessToken as string
+             session.backendAccessToken = token.backendAccessToken as string
         }
         return session
     }
